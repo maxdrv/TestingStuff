a117ebff3674f5799f01f463e850e6bf8f823482
MaximDerevnin <maxdrvgit@gmail.com>
2018-10-18 14:00:44 +0300

    Collection extra

---
+++ src/LearningNewStuff/Collections/CollectionTest.java
@@ -0,0 +1,14 @@
+package LearningNewStuff.Collections;
+
+import java.util.*;
+
+public class CollectionTest {
+    public static void main(String[] args) {
+        Collection<String> collection;
+        List<String> list;
+        Set<String> set;
+        Queue<String> queue;
+
+        Map<String, String> map;
+    }
+}
